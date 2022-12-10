f5a7349b99dcfbc61f604ca45dcc1f02e9c23d35
Flavio Ribeiro <flavioribeirorangel123@gmail.com>
2022-12-10 01:01:23 -0300

    Adicionado alguma funcionalidades

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const main = document.querySelector("main");
 const input = document.querySelector("input");
 const buttonAddElementInList = document.querySelector("button");
-const deleteElementButton = document.querySelector("#deleteList");
+const deleteListButton = document.querySelector("#deleteList");
 const ul = document.createElement("ul");
 
 ul.classList.add("list");
@@ -16,21 +16,36 @@ buttonAddElementInList.addEventListener("click", () => {
   li.innerText = input.value;
   li.classList.add("listElement");
 
+  const div = document.createElement("div");
+
   const deleteListElementButton = document.createElement("button");
   deleteListElementButton.innerText = "X";
-  deleteListElementButton.classList.add("delete");
+  deleteListElementButton.classList.add("deleteButton");
 
-  li.append(deleteListElementButton);
+  const markAsDone = document.createElement("button");
+  markAsDone.innerText = "V";
+  markAsDone.classList.add("doneButton");
+
+  div.append(markAsDone);
+  div.append(deleteListElementButton);
+  li.append(div);
   ul.prepend(li);
+
+  input.value = "";
 
   deleteListElementButton.addEventListener("click", () => {
     if (confirm("Deseja mesmo deletar este elemento?")) {
-      deleteListElementButton.parentElement.remove();
+      deleteListElementButton.parentElement.parentElement.remove();
     }
+  });
+
+  markAsDone.addEventListener("click", () => {
+    markAsDone.parentElement.parentElement.classList.toggle("markedAsDone");
+    markAsDone.classList.toggle("buttonDone");
   });
 });
 
-deleteElementButton.addEventListener("click", () => {
+deleteListButton.addEventListener("click", () => {
   if (confirm("Deseja deletar a lista por completo?")) {
     ul.innerHTML = ``;
   }
